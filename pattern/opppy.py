dc0813daede7da8575f0d077603b9d53576ee1f0
Narendhranath <113756607+naren004@users.noreply.github.com>
2024-03-28 15:50:54 +0530

    opppy.py

--- pattern/opppy.py
+++ pattern/opppy.py
@@ -1,5 +1,5 @@
-n=int(input("n="))
-for i in range(n):
-    print("*",end=" ")
-    for j in range(n+1):
-        print(" ",end="")
+n = int(input("Enter the number of rows: "))
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
